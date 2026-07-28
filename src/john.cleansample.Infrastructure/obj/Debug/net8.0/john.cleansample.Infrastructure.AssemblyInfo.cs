82d90dd4bdb44f361ff00a81cc664ad3932d8aaf
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2026-07-28 11:45:30 +0100

    updated

--- src/john.cleansample.Infrastructure/obj/Debug/net8.0/john.cleansample.Infrastructure.AssemblyInfo.cs
+++ src/john.cleansample.Infrastructure/obj/Debug/net8.0/john.cleansample.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("john.cleansample.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fffcc191c008db2e35bb8424fce49c9aa703641f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f99483b8184070f2488a5d0b40fd69911d85272")]
 [assembly: System.Reflection.AssemblyProductAttribute("john.cleansample.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("john.cleansample.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
